6925961bd404b4d12f25438604b4dbdc6165a978
tcoestudent <ansibleimage@gmail.com>
2026-07-22 03:23:31 -0700

    Update Main.java

--- myapp/Main.java
+++ myapp/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello I am in main function of java - change 1");
+        System.out.println("Hello I am in main function of java");
     }
 }
